a379f9ac9ce86c2693c92168b484233ddd5c5d7f
aidalucie <ndourlucie@gmail.com>
2019-11-08 15:35:38 +0100

    upload

---
+++ src/main/java/receiver/MyReceiver.java
@@ -0,0 +1,39 @@
+package receiver;
+
+import javax.jms.Message;
+import javax.jms.Queue;
+import javax.jms.QueueConnection;
+import javax.jms.QueueConnectionFactory;
+import javax.jms.QueueReceiver;
+import javax.jms.QueueSender;
+import javax.jms.QueueSession;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import javax.jms.Queue;
+import javax.jms.QueueConnectionFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyReceiver {
+
+	public static void main(String[] args) {
+		try{
+			
+			ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContextJMS.xml");
+			QueueConnectionFactory factory = (QueueConnectionFactory) applicationContext.getBean("connectionFactory");
+			
+			Queue queue = (Queue) applicationContext.getBean("queue");
+			
+			// Create a connection. See https://docs.oracle.com/javaee/7/api/javax/jms/package-summary.html
+			// Open a session	
+			// start the connection	
+			// Create a receive	
+			// Receive the message
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+}
